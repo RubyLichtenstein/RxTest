7effbb85d50e5fafb2cd2d673669181c004bd502
Ruby <reuven.lichtenstein@gmail.com>
2017-11-15 23:49:07 +0200

    fix test

--- src/test/kotlin/com/rubylichtenstein/rxkotlintest/CoreTest.kt
+++ src/test/kotlin/com/rubylichtenstein/rxkotlintest/CoreTest.kt
@@ -11,64 +11,6 @@ class CoreTest {
 
     fun <T> noValues() = valueCount<T>(0)
 
-    fun <T> oneOrTowValues() = valueCount<T>(1) or valueCount(2)
-
-    fun <T> moreValuesThen(count: Int)
-            = compose<T>({ it.values().size > count }, "Should have more values then $count")
-
-    fun <T> lessValuesThen(count: Int)
-            = compose<T>({ it.values().size < count }, "Should have less values then $count")
-
-    fun <T> valueCountBetween(min: Int, max: Int) = moreValuesThen<T>(min) and lessValuesThen<T>(max)
-
-    @Test
-    fun composeTest() {
-        val values = listOf<String>("Rx", "Kotlin", "Test")
-        Observable.fromIterable(values)
-                .test {
-                    it shouldHave moreValuesThen(2)
-                    it shouldHave noErrors()
-                    it shouldHave valueSequence(values)
-                }
-
-        Observable.empty<String>()
-                .test {
-                    it shouldHave noValues()
-                }
-
-        Observable.just("")
-                .test {
-                    it shouldHave oneOrTowValues()
-                }
-
-        Observable.just("","")
-                .test {
-                    it shouldHave valueCountBetween(1, 3)
-                }
-    }
-
-    @Test
-    fun assertionWrapperTest() {
-        val detailMessage = "error message"
-
-        val assertionWrapperPass
-                = assertionWrapper<String> {}
-
-        val assertionWrapper = assertionWrapper<String> {
-            throw AssertionError(detailMessage)
-        }
-
-        assertionWrapperPass.test(TestObserver()).passed shouldBe true
-        assertionWrapperPass.test(TestObserver()).message shouldBe passedMessage
-
-
-        assertionWrapper.test(TestObserver()).passed shouldBe false
-        assertionWrapper.test(TestObserver()).message shouldBe detailMessage
-    }
-}
-
-    fun <T> noValues() = valueCount<T>(0)
-
     fun <T> errorOrComplete(error: Throwable) = error<T>(error) or complete()
 
     fun <T> moreValuesThen(count: Int)
@@ -104,3 +46,24 @@ class CoreTest {
                     it shouldHave valueCountBetween(1, 3)
                 }
     }
+
+    @Test
+    fun assertionWrapperTest() {
+        val detailMessage = "error message"
+
+        val assertionWrapperPass
+                = assertionWrapper<String> {}
+
+        val assertionWrapper = assertionWrapper<String> {
+            throw AssertionError(detailMessage)
+        }
+
+        assertionWrapperPass.test(TestObserver()).passed shouldBe true
+        assertionWrapperPass.test(TestObserver()).message shouldBe passedMessage
+
+
+        assertionWrapper.test(TestObserver()).passed shouldBe false
+        assertionWrapper.test(TestObserver()).message shouldBe detailMessage
+    }
+}
+
