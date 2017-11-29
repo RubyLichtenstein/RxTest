b8433bfbe6a32d1e6cf68e01a5cb36b7d7b6f1c7
Ruby <reuven.lichtenstein@gmail.com>
2017-11-29 21:39:32 +0200

    minor test

--- src/test/kotlin/com/rubylichtenstein/rxkotlintest/extentions/ExtantionsTest.kt
+++ src/test/kotlin/com/rubylichtenstein/rxkotlintest/extentions/ExtantionsTest.kt
@@ -1,40 +1,40 @@
-package com.rubylichtenstein.rxkotlintest.extentions
-
-import com.rubylichtenstein.rxkotlintest.assertions.should
-import com.rubylichtenstein.rxkotlintest.matchers.complete
-import io.reactivex.Completable
-import io.reactivex.Maybe
-import io.reactivex.Observable
-import io.reactivex.Single
-import org.junit.Test
-
-
-internal class ExtensionsTest {
-
-    @Test
-    fun testTest() {
-        Observable.just("")
-                .test {
-                    it.assertComplete()
-                    it should complete()
-                }
-
-        Maybe.just("")
-                .test {
-                    it.assertComplete()
-                    it should complete()
-                }
-
-        Single.just("")
-                .test {
-                    it.assertComplete()
-                    it should complete()
-                }
-
-        Completable.complete()
-                .test {
-                    it.assertComplete()
-                    it should complete()
-                }
-    }
-}
+//package com.rubylichtenstein.rxkotlintest.extentions
+//
+//import com.rubylichtenstein.rxkotlintest.assertions.should
+//import com.rubylichtenstein.rxkotlintest.matchers.complete
+//import io.reactivex.Completable
+//import io.reactivex.Maybe
+//import io.reactivex.Observable
+//import io.reactivex.Single
+//import org.junit.Test
+//
+//
+//internal class ExtensionsTest {
+//
+//    @Test
+//    fun testTest() {
+//        Observable.just("")
+//                .test {
+//                    it.assertComplete()
+//                    it should complete()
+//                }
+//
+//        Maybe.just("")
+//                .test {
+//                    it.assertComplete()
+//                    it should complete()
+//                }
+//
+//        Single.just("")
+//                .test {
+//                    it.assertComplete()
+//                    it should complete()
+//                }
+//
+//        Completable.complete()
+//                .test {
+//                    it.assertComplete()
+//                    it should complete()
+//                }
+//    }
+//}
