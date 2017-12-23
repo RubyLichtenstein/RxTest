1f49dabf7aa64de307ce32581f9af2c87c06c366
Ruby <reuven.lichtenstein@gmail.com>
2017-12-23 22:30:17 +0200

    added detekt config

--- src/main/kotlin/com/rubylichtenstein/rxkotlintest/assertions/Assertions.kt
+++ src/main/kotlin/com/rubylichtenstein/rxkotlintest/assertions/Assertions.kt
@@ -6,16 +6,32 @@ import com.rubylichtenstein.rxkotlintest.matchers.value
 import io.reactivex.observers.BaseTestConsumer
 import org.hamcrest.*
 
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldHave(matcher: Matcher<BaseTestConsumer<T, U>>) = MatcherAssert.assertThat(this, matcher)
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldHave(matcher: Matcher<BaseTestConsumer<T, U>>)
+        = MatcherAssert.assertThat(this, matcher)
 
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.should(matcher: TestConsumerMatcher<T, U>) = assertThat<T, U>(this, matcher)
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldHave(matcher: TestConsumerMatcher<T, U>) = should(matcher)
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldBe(matcher: TestConsumerMatcher<T, U>) = should(matcher)
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldEmit(matcher: TestConsumerMatcher<T, U>) = should(matcher)
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldEmit(t: T) = shouldHave(value(t))
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldEmit(t: (T) -> Boolean) = shouldHave(value(t))
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldNeverEmit(t: T) = should(never(t))
-infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldNeverEmit(t: (T) -> Boolean) = should(never(t))
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.should(matcher: TestConsumerMatcher<T, U>)
+        = assertThat<T, U>(this, matcher)
+
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldHave(matcher: TestConsumerMatcher<T, U>)
+        = should(matcher)
+
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldBe(matcher: TestConsumerMatcher<T, U>)
+        = should(matcher)
+
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldEmit(matcher: TestConsumerMatcher<T, U>)
+        = should(matcher)
+
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldEmit(t: T)
+        = shouldHave(value(t))
+
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldEmit(t: (T) -> Boolean)
+        = shouldHave(value(t))
+
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldNeverEmit(t: T)
+        = should(never(t))
+
+infix fun <T, U : BaseTestConsumer<T, U>> BaseTestConsumer<T, U>.shouldNeverEmit(t: (T) -> Boolean)
+        = should(never(t))
 
 
 fun <T, U : BaseTestConsumer<T, U>> assertThat(actual: BaseTestConsumer<T, U>, matcher: TestConsumerMatcher<in T, in U>) {
