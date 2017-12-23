02b8f27c6ef74048cee58dc80d52b1df31cd6916
Ruby <reuven.lichtenstein@gmail.com>
2017-12-23 22:56:56 +0200

    it to explicit parameter

--- src/test/kotlin/com/rubylichtenstein/rxkotlintest/matchers/MatchersTest.kt
+++ src/test/kotlin/com/rubylichtenstein/rxkotlintest/matchers/MatchersTest.kt
@@ -122,11 +122,11 @@ class MatchersTest {
         to shouldHave valueAt(0, value0)
         to shouldHave valueAt(1, value1)
 
-        to.assertValueAt(0, { it == value0 })
-        to.assertValueAt(1, { it == value1 })
+        to.assertValueAt(0, { value -> value == value0 })
+        to.assertValueAt(1, { value -> value == value1 })
 
-        to shouldHave valueAt(0, { it == value0 })
-        to shouldHave valueAt(1, { it == value1 })
+        to shouldHave valueAt(0, { value -> value == value0 })
+        to shouldHave valueAt(1, { value -> value == value1 })
     }
 
     @Test
