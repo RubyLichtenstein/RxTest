66771a0d46d5568f72db04f37f8a88e74b7d1338
Ruby <reuven.lichtenstein@gmail.com>
2018-02-26 21:04:29 +0200

    added inline fun

--- src/main/kotlin/com/rubylichtenstein/rxtest/matchers/CreateMatcher.kt
+++ src/main/kotlin/com/rubylichtenstein/rxtest/matchers/CreateMatcher.kt
@@ -8,10 +8,10 @@ import io.reactivex.observers.BaseTestConsumer
  * by applying assertion on BaseTestConsumer
  *
  * @assertion assertion to apply on testConsumer,should return true on success.
- * @param failMessage to show in case the test not pass
+ * @param failMessage to show in case of failure
  */
-fun <T, U : BaseTestConsumer<T, U>> createMatcher(assertion: (BaseTestConsumer<T, U>) -> Boolean,
-                                                  failMessage: String)
+inline fun <T, U : BaseTestConsumer<T, U>> createMatcher(crossinline assertion: (BaseTestConsumer<T, U>) -> Boolean,
+                                                         failMessage: String)
         : Matcher<BaseTestConsumer<T, U>> {
     return object : Matcher<BaseTestConsumer<T, U>> {
         override fun test(value: BaseTestConsumer<T, U>)
@@ -31,18 +31,18 @@ fun <T, U : BaseTestConsumer<T, U>> createMatcher(assertion: (BaseTestConsumer<T
 }
 
 /**
- * Apply rx java assertion on testConsumer, delegating AssertionError as AssertionResult
+ * Apply native rx java assertion on testConsumer, returning rx native AssertionError as AssertionResult
  *
  * @param testConsumer to apply assertion on
- * @param assertion to apply on testConsumer - native RxJava assertions
+ * @param assertion native rx assertion to apply on testConsumer
  *
  * @return AssertionError? null on success
  */
-private fun <T, U : BaseTestConsumer<T, U>> applyAssertion(testConsumer: BaseTestConsumer<T, U>,
+inline private fun <T, U : BaseTestConsumer<T, U>> applyAssertion(testConsumer: BaseTestConsumer<T, U>,
                                                            assertion: (BaseTestConsumer<T, U>) -> Unit)
         : AssertionError? {
     return try {
-        assertion.invoke(testConsumer)
+        assertion(testConsumer)
         null
     } catch (assertionError: AssertionError) {
         assertionError
