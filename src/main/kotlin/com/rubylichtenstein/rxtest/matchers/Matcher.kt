bc99061a5192f798869f654948253f1da89b1c66
Ruby <reuven.lichtenstein@gmail.com>
2018-02-27 23:19:54 +0200

    codacy

--- src/main/kotlin/com/rubylichtenstein/rxtest/matchers/Matcher.kt
+++ src/main/kotlin/com/rubylichtenstein/rxtest/matchers/Matcher.kt
@@ -1,9 +1,18 @@
 package com.rubylichtenstein.rxtest.matchers
 
+/**
+ * Generic matcher with logical OR/AND
+ */
 interface Matcher<T> {
 
+    /**
+     * apply test
+     */
     fun test(value: T): Result
 
+    /**
+     * Logic AND
+     */
     infix fun and(other: Matcher<T>): Matcher<T> = object : Matcher<T> {
         override fun test(value: T): Result {
             val r = this@Matcher.test(value)
@@ -14,6 +23,9 @@ interface Matcher<T> {
         }
     }
 
+    /**
+     * Logic OR
+     */
     infix fun or(other: Matcher<T>): Matcher<T> = object : Matcher<T> {
         override fun test(value: T): Result {
             val r = this@Matcher.test(value)
@@ -25,4 +37,7 @@ interface Matcher<T> {
     }
 }
 
+/**
+ * Matcher test result
+ */
 data class Result(val passed: Boolean, val failMessage: String?)
